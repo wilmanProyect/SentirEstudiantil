6854722a10d3827f770e37a40a953ce878c29a3b
Nafta <161855796+wilmanProyect@users.noreply.github.com>
2025-03-23 22:35:15 -0400

    Header En el Principal

--- app/index.tsx
+++ app/index.tsx
@@ -57,7 +57,6 @@ const styles = StyleSheet.create({
 
   },
   input: {
-    backfaceVisibility: 'hidden',
     backgroundColor: 'rgba(255, 255, 255, 0.5)',
     padding: 10,
     borderRadius: 5,
--- app/principal.jsx
+++ app/principal.jsx
@@ -1,76 +1,109 @@
-import { Text, View, TextInput, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
-import useUserStore from '../store/userStore';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Text, View, TextInput, StyleSheet, ImageBackground, TouchableOpacity, Image } from 'react-native';
+
+
 export default function Principal() {
-    const { edad, setEdad } = useUserStore();
     const [contador, setContador] = useState(3);
+
     return (
         <ImageBackground
             source={require('../assets/images/background.jpg')}
             style={styles.background}
         >
+            <View style={styles.header}>
+                <Text style={styles.title}>Cielo emocional</Text>
+                <Image source={require('../assets/images/favicon.png')}></Image>
+            </View>
+            <View style={styles.header}>
+                <Text style={styles.subtitle}>¿Cómo te sientes el día de hoy?</Text>
+                <Text> __ </Text>
+            </View>
             <View style={styles.container}>
-                <Text style={styles.text}>Cielo Emocional</Text>
 
-                <Text style={styles.text}>¿Como te sientes el dia de hoy?</Text>
-
-
-                <TextInput
-                    placeholder="Edad"
-                    value={edad}
-                    onChangeText={setEdad}
-                    style={styles.input}
-                />
 
                 <TouchableOpacity
                     style={styles.button}
-                    onPress={() => router.push(`/principal`)}
+                    onPress={() => setContador(contador - 1)}
                 >
-                    <Text>Expresa aquí tus emociones</Text>
+                    <Text style={styles.buttonText}>Expresa aquí tus emociones</Text>
                 </TouchableOpacity>
-                <Text style={styles.text} >Aparecera una estrella en {contador}</Text>
+
+                <Text style={styles.counterText}>Aparecerá una estrella en {contador}</Text>
             </View>
-
         </ImageBackground>
-
     );
 }
+
 const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+        display: 'flex',
+    },
+    header2: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop:100,
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        resizeMode: 'cover'
+        padding: 20,
     },
     background: {
         flex: 1,
         resizeMode: 'cover',
         justifyContent: 'center',
     },
-
-    button: {
-        backfaceVisibility: 'hidden',
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
-        padding: 10,
-        borderRadius: 5,
-        marginTop: 20,
-
+    title: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center',
+        marginBottom: 10,
+        textDecorationLine: 'underline',
+    },
+    subtitle: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 20,
+        fontWeight: 'bold',
+    },
+    calendarContainer: {
+        marginBottom: 20,
+    },
+    calendarIcon: {
+        width: 50,
+        height: 50,
     },
     input: {
-        backfaceVisibility: 'hidden',
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
-        padding: 10,
-        borderRadius: 5,
-        width: '50%',
-        marginTop: 10,
-        marginBottom: 10,
-        textAlign: 'center'
-    },
-    text: {
-        color: 'white',
-        fontSize: 25,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        width: '80%',
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 20,
         textAlign: 'center',
-        paddingBottom: 20,
-        fontWeight: 'bold'
-    }
-});        
+        fontSize: 16,
+    },
+    button: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        padding: 15,
+        borderRadius: 10,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    counterText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center',
+        marginTop: 20,
+    },
+});
